ce504b5fc0707b25e7c5b846fdb82f6ea8b7f4ba
mung <yumengma@mungs-MacBook-Pro.local>
2024-03-04 10:36:59 -0500

    added sound

--- utils/SuperImage.js
+++ utils/SuperImage.js
@@ -1,16 +1,28 @@
 import { Animated, PanResponder } from 'react-native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
+import { Player } from '@react-native-community/audio-toolkit';
 
 class SuperImage {
   constructor(image) {
     this.image = image;
     this.pan = new Animated.ValueXY();
     this.canTriggerVibration = true;
+    this.soundUrl = "https://stellasonos-files.vercel.app/samples/bassoon/G1.mp3";
+    this.initialVolume = 0.5;
+    this.soundPlayer = new Player(this.soundUrl, {
+      autoDestroy: false
+    }).prepare((err) => {
+      if (!err) {
+        this.soundPlayer.volume = this.initialVolume;
+      }
+    });
+
     this.initPanResponder();
   }
 
   initPanResponder() {
     this.panResponder = PanResponder.create({
+      // activates PanResponder on movement gestures
       onMoveShouldSetPanResponder: () => true,
       onPanResponderMove: Animated.event(
         [null, { dx: this.pan.x, dy: this.pan.y }],
@@ -30,13 +42,16 @@ class SuperImage {
   play(x, y) {
     if (this.canTriggerVibration) {
       const style = Math.abs(x) < 100 && Math.abs(y) < 100 ? 'impactLight' : 'impactHeavy';
-      console.log(`Triggering Haptic with style: ${style}`); // Debug log
+      console.log(`Triggering Haptic with style: ${style}`); // debug
       this.triggerHaptic(style);
+      
+      // sound player
+      this.triggerSound();
 
       this.canTriggerVibration = false;
       setTimeout(() => {
         this.canTriggerVibration = true;
-      }, 1000); // Ensure this timeout matches or exceeds the library's internal cooldown, if any
+      }, 1000); 
     }
   }
 
@@ -46,7 +61,32 @@ class SuperImage {
       ignoreAndroidSystemSettings: false,
     });
   }
+
+  triggerSound() {
+    if (this.soundPlayer.isPlaying) {
+        console.log("Stopping sound..."); // log before stopping sound
+        this.soundPlayer.stop(() => {
+            console.log("Sound stopped."); // log after sound has stopped
+            this.soundPlayer.play(() => {
+                console.log("Sound started."); // log when sound starts again after stopping
+            });
+        });
+    } else {
+        console.log("Starting sound..."); // Log before playing sound for the first time
+        this.soundPlayer.play(() => {
+            console.log("Sound started."); // log when sound starts
+        });
+    }
+}
+
+  componentWillUnmount() {
+    // release
+    if (this.soundPlayer) {
+      this.soundPlayer.destroy();
+    }
+  }
 }
 
 export default SuperImage;
+
 
