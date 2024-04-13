3abbe538ccea943553262075c80017a72a20f548
mung <yumengma@mungs-MacBook-Pro.local>
2024-04-12 19:01:15 -0500

    filter + sound combined

--- pages/ImagePage.js
+++ pages/ImagePage.js
@@ -9,80 +9,159 @@ import {
   Text,
   Image,
   ImageBackground,
+  NativeModules,
   Pressable,
 } from "react-native";
 
-import { Grayscale } from "react-native-image-filter-kit";
+console.log("====================================");
+
+// This gets a blob of data from the PNG file written by
+// react-native-filter-kit.
+import RNFetchBlob from 'rn-fetch-blob';
+const { fs } = RNFetchBlob;
+
+// The blob appears to come as a base64 string. Buffer can decode that.
+import { Buffer } from 'buffer';
+
+// A library whose job to unpack a PNG-encoded blob.
+import * as png from '@vivaxy/png';
+
+import { Grayscale, Threshold, Brightness, GaussianBlur, BoxBlur, Sharpen,
+         cleanExtractedImagesCache} from "react-native-image-filter-kit";
 
 import SuperImage from '../utils/SuperImage.js';
 
 export default function ImagePage({ route, navigation }) {
-  const { image } = route.params; // Assuming 'image' is correctly provided from navigation params
+  const { image, name } = route.params;
+  // 'name' seems generally undefined here.
 
   // Create an instance of SuperImage
   const superImage = useRef(new SuperImage(image)).current;
 
   const pan = useRef(new Animated.ValueXY()).current;
 
+  const cursorState = useRef({ "prevX": 0,
+                               "prevY": 0,
+                               "prevTimeStamp": 0, }).current;
+
+  var blob;
+  var imageData;
+
   // calculate actual width and height of touch area
-  const xMax = Dimensions.get("window").width;
-  const yMax = Dimensions.get("window").height;
+  const touchAreaWidth = Dimensions.get("window").width;
+  const touchAreaHeight = Dimensions.get("window").height;
+
+  //const imageWidth = useRef(touchAreaWidth).current;
+  //const imageHeight = useRef(touchAreaHeight).current;
 
   return (
-    <View style={styles.container}>
-        <View
-          style={styles.imageContainer}
-          onStartShouldSetResponder={() => true}
-          onMoveShouldSetResponder={() => true}
-          onResponderTerminationRequest={() => false}
-          onResponderMove={(event) => {            
-            pan.setValue({
-              x: event.nativeEvent.pageX - 25,
-              y: event.nativeEvent.pageY + 25,
-            });
-            superImage.play(pan.x._value, pan.y._value);
-          }}
-          onResponderRelease={() => {
-            superImage.stopSound(); // Call stopSound when the finger is lifted
-          }}
-        >
-         <Image 
-            style={styles.image}
-            source={{ uri: superImage.currentImage().image.src }} 
-         />
-        <Animated.View
-          style={{
-            transformOrigin: 'top left',
-            transform: [
-              {
-                translateX: pan.x.interpolate({
-                  inputRange: [0, xMax],
-                  outputRange: [0, xMax],
-                  extrapolate: "clamp",
-                }),
-              },
-              {
-                translateY: pan.y.interpolate({
-                  inputRange: [0, 110, yMax-110, yMax],
-                  outputRange: [-yMax+200, -yMax+200, 0, 0],
-                  extrapolate: "clamp",
-                }),
-              },
-            ],
-          }}
-        >
-          <View style={styles.circle} />
-        </Animated.View>
-        </View>
+      <View
+    style={styles.imageContainer}
+    onResponderGrant={(event) => { 
+      // Look for a double click (half-second)
+      if (Math.abs(event.touchHistory.mostRecentTimeStamp - 
+                   cursorState.prevTimeStamp) < 500) {
+        navigation.goBack();
+      }
+
+      cursorState.prevX = event.nativeEvent.pageX;
+      cursorState.prevY = event.nativeEvent.pageY;
+      cursorState.prevTimeStamp = event.touchHistory.mostRecentTimeStamp;
+    } }
+    onStartShouldSetResponder={() => true}
+    onMoveShouldSetResponder={() => true}
+    onResponderTerminationRequest={() => false}
+    onResponderMove={(event) => {
+      pan.setValue({
+        x: event.nativeEvent.pageX,
+        y: event.nativeEvent.pageY,
+      });
+      superImage.play(pan.x._value, pan.y._value);
+    }} 
+    onResponderRelease={() => {
+      superImage.stopSound(); // Call stopSound when the finger is lifted
+    }}
+      >
+      <Sharpen
+    onFilteringError={ (event) => { console.log("+++",event); } }
+    onExtractImage={ (event) => { 
+
+      // event.nativeEvent.uri is the file written by filter-kit.
+      RNFetchBlob.fs.readFile(event.nativeEvent.uri, 'base64')
+        .then((data) => {
+          // Use Buffer to decode the base64 and png to get the image data.
+          imageData = png.decode(Buffer.from(data, 'base64'));
+
+          // Pass the image data to the superImage object.
+          superImage.performSegmentation(imageData);
+
+          // Should probably clean the cache here.
+          cleanExtractedImagesCache();
+        });
+    } }
+    extractImageEnabled={ true }
+    // image={<GaussianBlur
+    //        radius={ 1 }
+           image={
+               <Image
+             style={styles.image}
+             source={{uri: superImage.currentImage().src }}
+               />} 
+    //        />}
+    // amount={ 1 }
+      />
+      <Animated.View
+    style={{
+      transformOrigin: 'top left',
+      transform: [
+        {
+          translateX: pan.x.interpolate({
+            inputRange: [0, touchAreaWidth],
+            outputRange: [-touchAreaWidth/2, touchAreaWidth/2],
+            extrapolate: "clamp",
+          }),
+        },
+        {
+          translateY: pan.y.interpolate({
+            inputRange: [0, 0, touchAreaHeight, touchAreaHeight],
+            outputRange: [-touchAreaHeight+75, -touchAreaHeight+75, 75, 75],
+            extrapolate: "clamp",
+          }),
+        },
+      ],
+    }}
+    onStartShouldSetResponder={(event) => {
+      false }}
+    onMoveShouldSetResponder={(evt) => false }
+    onResponderReject={(event) => {}}
+    onResponderGrant={(event) => {}}
+      >
+      <View style={styles.circle} />
+      </Animated.View>
       </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  imageContainer: {
     flex: 1,
-    alignItems: "center",
+    backgroundColor: "#266",
+    margin: 0,
+    padding: 50,
+    borderWidth: 12,
+    borderColor: "#F0A",
+    position: "absolute",
+    top: 0,
+    left: 0, 
+    bottom: 0,
+    right: 0,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    flex: 1,
+    resizeMode: "contain",
+    width: Dimensions.get("window").width,
   },
   circle: {
     height: 40,
@@ -90,26 +169,4 @@ const styles = StyleSheet.create({
     backgroundColor: "pink",
     borderRadius: 50,
   },
-  imageContainer: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height / 1.3,
-    backgroundColor: "#000",
-    margin: 0,
-    zIndex: -1,
-    elevation: -1,
-    position: "absolute",
-    borderWidth: 2,
-    borderColor: "#F0F"
-  },
-  image: {
-    flex: 1,
-    width: null,
-    height: null,
-    margin: 0,
-    maxHeight: "100%",
-    maxWidth: "100%",
-  },
 });
-
-ImagePage.js
-
--- utils/SuperImage.js
+++ utils/SuperImage.js
@@ -1,37 +1,84 @@
-import { Animated } from 'react-native';
+import { Animated, Dimensions } from 'react-native';
 import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 import { Player } from '@react-native-community/audio-toolkit';
 
+import * as _Jimp from 'jimp';
+const Jimp = (typeof self !== 'undefined') ? (self.Jimp || _Jimp) : _Jimp;
+
+// Class to hold a single image, meant to be part of a stack of
+// images, in the 'SuperImage' class.
 class SubImage {
-  constructor(image) {
-    console.log("initializing SubImage");
-    this.image = image; // full image object passed from ImagePage
+
+  constructor(src, description) {
+    console.log("initializing SubImage", src, description);
+    this.src = src;
+    this.description = description;
   }
 
   clone = async function() {
-    return new SubImage(this.image);
+    return(new SubImage(this.src, this.description));
   }
 }
 
+
+// This class is meant to hold a whole collection of SubImages, each
+// corresponding to some component of an astronomical object. So we
+// can choose which one to display. The 'image' object it accepts is 
+// an entry in the images.json file, and it contains a master URL, and
+// also a collection of subImage URLs.
 class SuperImage {
-  constructor(image, name = "") {
-    var subImage = new SubImage(image);
-    var imageKey = name;
-    if (imageKey === "") imageKey = "1";
-    this.images = {}; // a collection of SubImage objects
-    this.images[imageKey] = subImage; // Load the first one
-    console.log("initializing SuperImage", Object.keys(this.images));
-    this.currentImageKey = Object.keys(this.images)[0];
+  constructor(complexImage, name="") {
+
+    console.log("initializing SuperImage", Object.keys(complexImage));
+    this.masterSrc = complexImage.src;
+    this.URL = complexImage.url;
+    this.description = complexImage.description
+    this.id = complexImage.id;
+    this.layers = new Array();
+
+    for (var i = 0; i < complexImage.layers.length; i++) {
+      this.layers[complexImage.layers[i].id] = 
+        new SubImage(complexImage.layers[i].src, complexImage.layers[i].layer);
+    }
+
+    // Start at the first image key.
+    this.currentImageKey = Object.keys(this.layers)[0];
+    console.log("complexImage starting with:", this.currentImageKey);
     
-    this.segmentRecord = { sum: 0, color: 0, haptic: 0, sound: 0 };
+    // This will be our segmentation record. Ultimately, the segmented
+    // image should just be another SubImage in the layers array, but
+    // later.
+    this.segmentData = null;
+
+    // A single segment record corresponds to a region of the
+    // segmented image. It records a measure of the segment's size and
+    // color, and also an index into the tables for the sound and
+    // vibrations to be played for that segment. This is a prototype
+    // record to be used as a model. It is also the empty record
+    // returned when there is no object at that pixel position. 
+    this.segmentRecord = {sum: 0, color: 0, haptic: 0, sound: 0};
+    
+    // This is for processing the floodFill used in segmentation.
+    this.visited = null;
+
+    // This is a collection of records corresponding to the objects in
+    // an image. The zero record is the empty record.
     this.segmentRecords = new Map();
     this.segmentRecords.set(0, Object.assign({}, this.segmentRecord));
-    
-    this.pan = new Animated.ValueXY();
-    this.canTriggerVibration = true;
-    this.soundUrl = "https://stellasonos-files.vercel.app/samples/bassoon/G1.mp3";
-    this.initialVolume = 0.5;
 
+    this.pan = new Animated.ValueXY(); // handling drag movements
+    this.canTriggerVibration = true; // control vibration feedback frequency
+    this.soundUrl = "https://stellasonos-files.vercel.app/samples/bassoon/G1.mp3"; //url for sound effect
+    this.initialVolume = 0.5; // initial sound volume
+    this.soundPlayer = new Player(this.soundUrl, {
+      autoDestroy: false
+    }).prepare((err) => {
+      if (!err) {
+        this.soundPlayer.volume = this.initialVolume;
+      }
+    });
+
+    /* new */
     // Initialize two sound players
     this.playerOne = new Player(this.soundUrl, { autoDestroy: false }).prepare();
     this.playerTwo = new Player(this.soundUrl, { autoDestroy: false }).prepare();
@@ -45,40 +92,252 @@ class SuperImage {
     this.isPlaying = false; // New state control
     this.switchInterval = 5000; // Time between automatic player switches
 
-    this.performSegmentation();
+    // placeholder for segmented image data
+    /// this.layers['segmented'] = null;
   }
 
-  addImage(image, name = "") {
+  addImage(image, name="") {
     var imageKey = name;
-    if (imageKey === "") imageKey = Object.keys(this.images).length.toString();
-    this.images[imageKey] = new SubImage(image);
+    if (imageKey == "") imageKey=this.layers.length.toString();
+    
+    this.layers[imageKey] = image;
   }
 
   currentImage() {
-    return this.images[this.currentImageKey];
+    return(this.layers[this.currentImageKey]);
   }
 
-  performSegmentation() {
-    console.log("Performing segmentation on the image");
-    // Segmentation logic here
+  // Accept window coordinates, return image coordinates.
+  getPos(x, y) { 
+    var yr = (this.win.totalHeight - this.win.winHeight)/2;
+    return { x: Math.round((x / this.win.winWidth) * this.win.imgWidth),
+             y: Math.round(((y - yr)/ this.win.winHeight) * this.win.imgHeight),
+           };
+    }
+
+
+  // Given a bit position [i,j], this function does a flood fill on
+  // all the connected area, setting all those pixels to the input
+  // 'value' argument. The image being operated on is this.segmentedData.
+  floodFill(i, j, value) {
+    if ((i < 0) || (i >= this.win.imgWidth) || 
+        (j < 0) || (j >= this.win.imgHeight)) return(0);
+
+    var fillStack = [];
+
+    fillStack.push([i, j]);
+    var output = 0;
+
+    while(fillStack.length > 0) {
+      // Check the top of the stack.
+      var [icol, jrow] = fillStack.pop();
+      var index = jrow * this.win.imgWidth + icol;
+
+      // Check to see if we need to bother with this cell. Either the cell 
+      // is out of bounds, or was never our business, or it's already been 
+      // visited.
+      if ((icol < 0) || 
+          (icol >= this.win.imgWidth) || 
+          (jrow < 0) || 
+          (jrow >= this.win.imgHeight) ||
+          (this.segmentData[index] < 0) || 
+          (this.visited[index] == true) ||
+          (this.segmentData[index] == value)) continue;
+
+      // Apparently we need to. Record the cell value in the image...
+      this.segmentData[index] = value;
+
+      // ... record that we have visited this cell...
+      this.visited[index] = true;
+
+      // ... increment the output counter, and ...
+      output += 1;
+
+      // ... push the neighboring cells onto the stack.
+      if (jrow > 0) fillStack.push([icol, jrow-1]);
+      if (jrow <= this.win.imgHeight) fillStack.push([icol, jrow+1]);
+      if (icol > 0) fillStack.push([icol-1, jrow]);
+      if (icol <= this.win.imgWidth) fillStack.push([icol+1, jrow]);
+    }
+
+    return(output);
   }
 
-  play(x, y) {
-    this.isPlaying = true; // Set isPlaying to true to allow sound
-    console.log("playing at:", x.toFixed(2), y.toFixed(2));
-    const style = Math.abs(x) < 100 && Math.abs(y) < 100 ? 'impactLight' : 'impactHeavy';
-    if (this.canTriggerVibration) {
-      ReactNativeHapticFeedback.trigger(style, {
-        enableVibrateFallback: true,
-        ignoreAndroidSystemSettings: false,
+  async performSegmentation(imageData) {
+
+    // What are the actual dimensions of the displayed image, in pixels.
+    this.win = {
+      winWidth: Dimensions.get("window").width,
+      winHeight: (imageData.height / imageData.width) * 
+        Dimensions.get("window").width,
+      totalHeight: Dimensions.get("window").height,
+      imgWidth: imageData.width,
+      imgHeight: imageData.height,
+    }
+
+    this.segmentData = null;
+
+    console.log("Window thing:", this.win);
+
+    await new Jimp(
+      imageData.width, imageData.height, 
+      (error, jData) => { 
+        console.log("JIMP success ***!", error, Object.keys(imageData),Object.keys(jData.bitmap), jData.bitmap.width, jData.bitmap.height, typeof jData.bitmap.data[1], imageData.colorType, imageData.sRGB, imageData.data.length);
+        if (error) stop(error);
+
+        // Build a convolution array.
+        var kernelArray = Array.from(Array(5), () => new Array(5));
+        kernelArray[0] = [1/25, 1/25, 1/25, 1/25, 1/25];
+        kernelArray[1] = [1/25, 1/25, 1/25, 1/25, 1/25];
+        kernelArray[2] = [1/25, 1/25, 1/25, 1/25, 1/25];
+        kernelArray[3] = [1/25, 1/25, 1/25, 1/25, 1/25];
+        kernelArray[4] = [1/25, 1/25, 1/25, 1/25, 1/25];
+        
+        // res is just for debugging, remove it when you want.
+        var res
+
+        // 'result' is for transferring the output data to this.segmentData. 
+        // 'this' refers to something else inside the functions below.
+        var result = new Int16Array(jData.bitmap.width * jData.bitmap.height);
+        jData.scan(
+          0, 0, jData.bitmap.width, jData.bitmap.height, 
+          function (x, y, idx) {
+            // x, y is the position of this pixel on the image idx is
+            // the start position of this rgba tuple in the bitmap.
+            this.bitmap.data[idx + 0] = imageData.data[idx + 0]; // red
+            this.bitmap.data[idx + 1] = imageData.data[idx + 1]; // green 
+            this.bitmap.data[idx + 2] = imageData.data[idx + 2]; // blue
+            this.bitmap.data[idx + 3] = imageData.data[idx + 3]; // alpha
+
+            if (x == jData.bitmap.width - 1 && y == jData.bitmap.height - 1) {
+              // image scan finished, do first segmentation step.
+              res = jData
+                .clone()
+                .resize(500, 500)
+                .blur(5)
+                .convolute(kernelArray)
+                .resize(imageData.width, imageData.height)
+                .greyscale()
+              // Apply a threshold and inversion, while copying to output.
+                .scan(0, 0, jData.bitmap.width, jData.bitmap.height, 
+                      function(x, y, idx) {
+                        result[y * jData.bitmap.width + x] =
+                          this.bitmap.data[idx] > 50 ? 0 : -1;
+                      });
+            }
+
+          });
+
+        this.segmentData = result;
+
+        // console.log(">>>>>>>>>", 
+        //             imageData.data[6080],
+        //             imageData.data[6081],
+        //             imageData.data[6082],
+        //             imageData.data[6083],
+        //             res.bitmap.data[6080],
+        //             res.bitmap.data[6081],
+        //             res.bitmap.data[6082],
+        //             res.bitmap.data[6083],
+        //             this.segmentData[1520]);
+        // console.log(">>>>>>>>>", 
+        //             imageData.data[1500],
+        //             imageData.data[1501],
+        //             imageData.data[1502],
+        //             imageData.data[1503],
+        //             res.bitmap.data[1500],
+        //             res.bitmap.data[1501],
+        //             res.bitmap.data[1502],
+        //             res.bitmap.data[1503],
+        //             this.segmentData[375]);
+
+        // Other segmentation work goes here.
+
+
+        this.visited = new Uint8Array(this.segmentData.length).fill(false);
+
+        var nextObject = 0;
+        var index = 0;
+        for (let i = 0; i < this.win.imgWidth; i++) {
+          for (let j = 0; j < this.win.imgHeight; j++) {
+        
+            index = j * this.win.imgWidth + i;
+
+            if (this.segmentData[index] == 0) {
+              // We have an object we probably we haven't seen it before...
+          
+              // ... double check about not seeing it before.
+              if (this.visited[index] == false) {
+
+                console.log("found one", i, j, nextObject);
+
+                nextObject += 1;
+                var objectID = nextObject;
+
+                // Record some results in the segmentData record.
+                if (!this.segmentRecords.has(objectID)) {
+                  this.segmentRecords.set(objectID,
+                                          {sum: this.segmentRecord.sum,
+                                           color: this.segmentRecord.color,
+                                           color: this.segmentRecord.haptic,
+                                           color: this.segmentRecord.sound });
+                }
+
+                // Fill the object, return
+                this.segmentRecords.get(objectID).sum = 
+                  this.floodFill(i, j, objectID);
+
+                console.log("segmentation record:", objectID, 
+                            this.segmentRecords.get(objectID).sum);
+                // As of here, we have a rough estimate of the size of an
+                // object, so this is a place where we can put logic to
+                // assign different notes or haptic values based on size.
+
+              }
+            }
+          }
+        }
+
+        console.log("Done with segmentation, ready to rock.");
       });
+
+  }
+
+  // Retrieve one or more of the segment records corresponding to this
+  // screen position. These records will be used to decide what sounds
+  // and vibrations to play.
+  getRecords(x, y) {
+    console.log("getting segmentation records");
+  }
+
+  // Depending on where the user interacts with the image (determined
+  // by the gesture's x, y coordinates) to trigger different sounds or
+  // haptic feedback based on segment
+  play(x, y) {
+    /* new */
+    this.isPlaying = true; // Set isPlaying to true to allow sound
+    // Check segmentation data
+    let pos = this.getPos(x, y)
+    console.log("playing at:", pos.x, pos.y,
+                this.segmentData[ pos.y * this.win.imgWidth + pos.x ]);
+
+    const style = Math.abs(x) < 100 && Math.abs(y) < 100 ? 'impactLight' : 
+          'impactHeavy';
+    if (this.canTriggerVibration) {
+      console.log(`Triggering Haptic with style: ${style}`);
+      this.triggerHaptic(style);
       this.canTriggerVibration = false;
       setTimeout(() => {
         this.canTriggerVibration = true;
-      }, 1000); // Reset haptic trigger flag after a delay
+      }, 1000); // reset haptic trigger flag after a delay
     }
 
+    // if (!this.soundPlayer.isPlaying) {
+    //   console.log("Starting sound..."); // log before playing sound
+    //   this.triggerSound();
+    // }
     if (!this.activePlayer.isPlaying && this.isPlaying) {
+      console.log("Starting sound..."); // log before playing sound
       this.activePlayer.play();
       this.scheduleSwitch();
     }
@@ -108,9 +367,18 @@ class SuperImage {
     clearTimeout(this.switchTimer); // Ensure no player switch occurs after stopping
   }
 
+  triggerHaptic(style) {
+    ReactNativeHapticFeedback.trigger(style, {
+      enableVibrateFallback: true,
+      ignoreAndroidSystemSettings: false,
+    });
+    console.log(`Haptic feedback triggered: ${style}`);
+  }
+
   componentWillUnmount() {
-    this.playerOne.destroy();
-    this.playerTwo.destroy();
+    if (this.soundPlayer) {
+      this.soundPlayer.destroy();
+    }
   }
 }
 
